204a927d78be8f309a2589e4be66954a9e2ba3a6
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-10-14 23:04:20 +0000

    test: add testing template for the abstracted storage engine

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""This module tests the file_storage module
+from the engine package
+"""
+
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models import storage
+import unittest
+
+
+class TestFileStorage(unittest.TestCase):
+    """Defines test cases for the FileStorage class"""
+
+    # Show the whole output when an assert method fails
+    maxDiff = None
+
+    def test_new_filestorage_obj(self):
+        pass
+
+    def test__file_path(self):
+        pass
+
+    def test__objects(self):
+        pass
+
+    def test_all_method(self):
+        pass
+
+    def test_save_method(self):
+        pass
+
+    def test_reload_method(self):
+        pass
+
+    def test_with_new_basemodel_obj(self):
+        pass
+
+    def test_when_basemodel_obj_saved(self):
+        pass
