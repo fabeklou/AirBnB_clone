9b2f91bba479a1779eee6b44caca823e7e74406d
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-10-15 00:37:37 +0000

    refactor: init method to set date first

--- models/base_model.py
+++ models/base_model.py
@@ -35,9 +35,9 @@ class BaseModel:
 
         """
         if not kwargs:
-            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            self.id = str(uuid.uuid4())
             storage.new(self)
             return
         for attr, value in kwargs.items():
@@ -84,5 +84,5 @@ class BaseModel:
         iso_updated = dt_updated_at.isoformat("T", "auto")
 
         dict_attrs.update(__class__=class_name,
-                          created_at=iso_updated, updated_at=iso_created)
+                          updated_at=iso_updated, created_at=iso_created)
         return dict_attrs
